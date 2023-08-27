1367d5586aa0284fdbf3a69e1068278a990e25bf
piyush7833 <96736508+piyush7833@users.noreply.github.com>
2023-08-27 11:03:15 +0530

    fixing cors using cgpt

--- src/index.js
+++ src/index.js
@@ -2,24 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { store,persistor } from './redux/store';
+import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://iiitusnapshotbackend.onrender.com/api'
+axios.defaults.baseURL = 'https://iiitusnapshotbackend.onrender.com/api';
 axios.defaults.withCredentials = true;
-axios.defaults.headers.post['mode']='cors'
-axios.defaults.headers.post['Access-Control-Allow-Origin']='*'
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-    <App />
-    </PersistGate>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
